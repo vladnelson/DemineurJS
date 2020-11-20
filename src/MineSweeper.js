c4803fd773e9661f25b21de1659748d13f5827f2
Lucas Michelin <49991508+LucasBuuck@users.noreply.github.com>
2020-11-20 18:54:25 +0100

    MineSweeper Init
    we can make the logic ground

--- src/MineSweeper.js
+++ src/MineSweeper.js
@@ -1,3 +1,69 @@
-(()=>{
+(() => {
+  const SIZE_LENGHT = 20;
+  const SIZE_HEIGHT = 20;
+  const NUMBER_MINE = 5;
 
-})()
+  groundPositions = [SIZE_LENGHT][SIZE_HEIGHT];
+
+  window.stateBtn = {
+    constructor(a,b) {
+      isClicked = a 
+      isMine = b; 
+    }    
+  }
+
+  function initGround(){
+    createGround();
+    putMines();
+  }
+
+
+  function createGround() {
+    for(let i = 0; i< SIZE_LENGHT; i++){
+      for(let j = 0; j< SIZE_HEIGHT; j++){
+       groundPositions[i][j] = new StateBtn(false,false);
+      }
+    }
+  }
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); 
+  }
+
+  function putMines(){
+    for(let i = 0; i < NUMBER_MINE;i ++ ){
+      let a = getRandomInt(0,SIZE_LENGHT);
+      let b = getRandomInt(0,SIZE_HEIGHT);
+      if(groundPositions[a][b].isMine === true)i--;
+      else groundPositions[a][b].isMine = true;
+    } 
+  }
+  
+  
+    window.MineSweeper = {
+       States :  { 
+         
+        InitGame :  () =>{
+          const me  = {};
+          
+          me.click = (x,y) => {
+            arrayA[x,y] = true;
+          }
+          me.setNextOp = _ => me;
+        },
+        OnGame :  () =>{
+          const me  = {};
+          
+
+        },
+        EndGame :  () =>{
+          const me  = {};
+          
+        }
+      }
+
+
+    }
+  })();
